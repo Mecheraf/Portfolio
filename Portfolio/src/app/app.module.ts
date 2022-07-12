6fd436efb4ffd0e82f7b90fce885f466cb4e6dbf
alan <mecheraf@gmail.com>
2022-07-13 00:28:46 +0200

    json done

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -9,12 +9,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { TrainingsComponent } from './trainings/trainings.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ExperiencesComponent,
+    TrainingsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
