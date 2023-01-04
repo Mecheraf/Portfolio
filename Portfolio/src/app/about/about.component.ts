8759f8c925e794c1f4b1b8bc84fdffd9ea759313
Mecheraf <mecheraf@gmail.com>
2023-01-04 19:22:45 +0100

    test

--- Portfolio/src/app/about/about.component.ts
+++ Portfolio/src/app/about/about.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -4,8 +4,7 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Portfolio';
