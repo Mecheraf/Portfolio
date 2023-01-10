90014b5ec60821bed03b5b8a94c0ef6360cb6f88
alan <mecheraf@gmail.com>
2023-01-10 15:42:51 +0100

    Nothing to commit

--- Portfolio/src/app/shared/header/header.component.ts
+++ Portfolio/src/app/shared/header/header.component.ts
@@ -30,7 +30,6 @@ export class HeaderComponent implements OnInit {
     this.translate.use(lang);
   }
 
-  @HostListener('window:resize', ['$event'])
   getWindowSize() {
     this.isMobile = window.innerWidth < 768;
   }
