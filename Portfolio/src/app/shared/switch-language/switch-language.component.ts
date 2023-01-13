5fa792e91853cdf6cf46be56dadcb6b2a36d5533
Mecheraf <mecheraf@gmail.com>
2023-01-13 17:16:02 +0100

    not done yet

--- Portfolio/src/app/shared/switch-language/switch-language.component.ts
+++ Portfolio/src/app/shared/switch-language/switch-language.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+
+
 
 
 @Component({
@@ -8,17 +11,23 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./switch-language.component.scss']
 })
 export class SwitchLanguageComponent implements OnInit {
-
-  constructor(public translate: TranslateService) {
+  email:string = "alan.mecheraf@gmail.com"
+  constructor(public translate: TranslateService, public clipboard: Clipboard) {
     translate.addLangs(['en', 'fr', 'jp']);
     translate.setDefaultLang('fr');
+    this.translate.currentLang = 'fr'
   }
 
   switchLanguage(lang: string){
     this.translate.use(lang);
+    console.log(this.translate.currentLang)
   }
 
   ngOnInit(): void {
+  }
+
+  public copyToClipboard(): void {
+    const successful = this.clipboard.copy(this.email)
   }
 
 }
