735ca489e95b3a710910aa267fdd29445e5120e3
Alan Mecheraf <mecheraf@gmail.com>
2025-06-03 13:17:17 +0200

    Added meta title and description

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,21 @@ export class AppComponent {
   title = 'Portfolio';
 
   constructor(
-    public translate: TranslateService
+    public translate: TranslateService,
+    private meta: Meta
   ){
     translate.addLangs(['en', 'fr', 'jp']);
     translate.setDefaultLang('fr');
+
+    this.meta.addTag({
+      name: 'title',
+      content: 'Alan Mecheraf - Portfolio'
+    });
+    
+    this.meta.addTag({
+      name: 'description',
+      content: 'Alan Mecheraf; DevOps, Développeur web et mobile expérimenté. Président de l\'association franco-japonaise Gengoffee.'
+    });
   }
 
   switchLanguage(lang: string){
