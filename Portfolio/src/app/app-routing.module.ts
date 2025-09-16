eaa2572cc2688eab74a7f1e269665eae0c9ffb4c
Alan Mecheraf <mecheraf@gmail.com>
2025-09-16 11:20:19 +0200

    Updated some text

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AnnisrayanbourefisComponent } from './easters/annisrayanbourefis/annisrayanbourefis.component';
 import { BodyComponent } from './body/body.component';
-import { DainjungComponent } from './easters/dainjung/dainjung.component';
 
 const routes: Routes = [
   {path :'', component: BodyComponent},
   {path :'easters/annisrayanbourefis', component: AnnisrayanbourefisComponent},
-  {path : 'easters/dainjung', component: DainjungComponent},
   {path : '**', redirectTo : ''}
 ];
 
