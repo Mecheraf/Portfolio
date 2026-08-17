1ed616f8bf5adbfb3e596271e3e5cfcfded288c8
Alan Mecheraf <mecheraf@gmail.com>
2026-08-17 17:27:47 +0200

    Update angular cli 21 -> 22

--- Portfolio/src/app/easters/annisrayanbourefis/annisrayanbourefis.component.ts
+++ Portfolio/src/app/easters/annisrayanbourefis/annisrayanbourefis.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CountdownConfig, CountdownModule } from 'ngx-countdown';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-annisrayanbourefis',
